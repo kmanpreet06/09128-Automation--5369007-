5cc8334bc31cca830fde8568eef45371707599c5
kmanpreet06 <99104159+kmanpreet06@users.noreply.github.com>
2022-02-05 19:08:03 -0600

    Add files via upload

---
+++ assignment.py
@@ -0,0 +1,13 @@
+print("i am taking name from the user ")
+
+firstname = input("enter the first name:")
+middlename = input("enter the middle name")
+lastname = input("enter the last name:" )
+fullname = "manpreet kaur dhillon"
+fullname.split()
+first = fullname.split()[0]
+middle = fullname.split()[-2]
+last = fullname.split()[-1]
+print("Last name is: " + last)
+print("Middle name is: " + middle)
+print("First name is: " + first)
